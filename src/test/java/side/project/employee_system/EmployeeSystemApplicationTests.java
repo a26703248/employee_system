528a446be5563051eddae53481b109e09df4231d
a26703248 <a0909007892@gmail.com>
2022-12-23 23:40:53 +0800

    init

---
+++ src/test/java/side/project/employee_system/EmployeeSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package side.project.employee_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
