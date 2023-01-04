bc2b95d60e3fa0f26a5a6c309f4c77d2459fbcf1
a0909007892 <a0909007892@gmail.com>
2023-01-04 16:54:19 +0800

    init project

--- src/main/java/side/project/employee_system/EmployeeSystemApplication.java
+++ src/main/java/side/project/employee_system/EmployeeSystemApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmployeeSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmployeeSystemApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(EmployeeSystemApplication.class, args);
+  }
 
 }
