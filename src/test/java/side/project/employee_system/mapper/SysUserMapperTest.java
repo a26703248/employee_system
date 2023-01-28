11491fdf93e6b538b034b1a8e93a1c83b08b5462
a0909007892 <a0909007892@gmail.com>
2023-01-28 17:11:58 +0800

    add mapper test

---
+++ src/test/java/side/project/employee_system/mapper/SysUserMapperTest.java
@@ -0,0 +1,38 @@
+package side.project.employee_system.mapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SysUserMapperTest {
+
+  @Test
+  public void insertTest() {
+
+  }
+
+  @Test
+  public void selectTest() {
+
+  }
+
+  @Test
+  public void updateTest() {
+
+  }
+
+  @Test
+  public void deleteTest() {
+
+  }
+
+  @Test
+  public void getNavMenuIdTest() {
+
+  }
+
+  @Test
+  public void getByUsernameTest() {
+
+  }
+}
